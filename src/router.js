488b68c4b83396e7b910dbe62733764388ede892
Alexey <Zimin>
2019-02-15 10:05:47 +0300

    FEAT sandBox arturKe

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from './views/Home'
+import sandbox from './components/sandbox/HelloWorldArturKe'
 
 Vue.use(Router)
 
@@ -17,7 +18,15 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/About')
+    },
+    {
+      path: '/sandbox',
+      name: 'sandbox',
+      props: {
+        msg: 'Hey, ArturKe! :)'
+      },
+      component: sandbox
     }
   ]
 })
